0cd1828b7f613ff4bf21b6361bbad10fe6b77dbe
Tyll'a <psduckie@yahoo.com>
2024-08-08 04:31:56 -0400

    Implemented the sound playback
    This one actually works!

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const tmi = require("tmi.js");
+const player = require("node-wav-player");
 
 const client = new tmi.Client({
     connection: {
         secure: true,
         reconnect: true
     },
-    channels: [ "tylla" ]
+    channels: ["tylla"]
 });
 
 client.connect();
 
 client.on("message", (channel, tags, message, self) => {
     console.log("Meow");
+    player.play({
+        path: './meow.wav',
+    }).then(() => {
+        console.log('The wav file started to be played successfully.');
+    }).catch((error) => {
+        console.error(error);
+    });
 });
