c30e3e7e19d81944566edb7c9b8904330654c9c0
Tyll'a <psduckie@yahoo.com>
2025-01-03 21:11:00 -0500

    Made variables private

--- Bot.cs
+++ Bot.cs
@@ -7,28 +7,31 @@ using TwitchLib.Client.Extensions;
 using TwitchLib.Client.Models;
 using TwitchLib.Communication.Clients;
 using TwitchLib.Communication.Models;
+using Discord.WebSocket;
 
 namespace CoraCatte
 {
     public class Bot
     {
-        TwitchClient client;
-        static XmlDocument config;
-        string twitchUsername;
-        string twitchChannel;
-        string twitchAccessToken;
+        private static XmlDocument config;
+        private static TwitchClient client;        
+        private static string twitchUsername;
+        private static string twitchChannel;
+        private static string twitchAccessToken;
 
-        public string TwitchUsername { get => twitchUsername; set => twitchUsername = value; }
-        public string TwitchChannel { get => twitchChannel; set => twitchChannel = value; }
-        public string TwitchAccessToken { get => twitchAccessToken; set => twitchAccessToken = value; }
+        public static XmlDocument Config { get => config; set => config = value; }
+        public static TwitchClient Client { get => client; set => client = value; }
+        public static string TwitchUsername { get => twitchUsername; set => twitchUsername = value; }
+        public static string TwitchChannel { get => twitchChannel; set => twitchChannel = value; }
+        public static string TwitchAccessToken { get => twitchAccessToken; set => twitchAccessToken = value; }
 
         public Bot()
         {
-            config = new XmlDocument();
-            config.Load("config.xml");
-            TwitchUsername = config.SelectSingleNode("config/twitch.username").InnerText;
-            TwitchChannel = config.SelectSingleNode("config/twitch/channel").InnerText;
-            TwitchAccessToken = config.SelectSingleNode("config/twitch/accessToken").InnerText;
+            Config = new XmlDocument();
+            Config.Load("config.xml");
+            TwitchUsername = Config.SelectSingleNode("config/twitch.username").InnerText;
+            TwitchChannel = Config.SelectSingleNode("config/twitch/channel").InnerText;
+            TwitchAccessToken = Config.SelectSingleNode("config/twitch/accessToken").InnerText;
 
             ConnectionCredentials credentials = new ConnectionCredentials(TwitchUsername, TwitchAccessToken);
             var clientOptions = new ClientOptions
@@ -37,10 +40,10 @@ namespace CoraCatte
                 ThrottlingPeriod = TimeSpan.FromSeconds(30)
             };
             WebSocketClient customClient = new WebSocketClient(clientOptions);
-            client = new TwitchClient(customClient);
-            client.Initialize(credentials, TwitchChannel);
+            Client = new TwitchClient(customClient);
+            Client.Initialize(credentials, TwitchChannel);
 
-            client.OnMessageReceived += Client_OnMessageReceived;
+            Client.OnMessageReceived += Client_OnMessageReceived;
         }
 
         private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
