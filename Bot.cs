946b06e8cb8126aff41a816623c99a18619d8f9e
Tyll'a <psduckie@yahoo.com>
2025-01-03 21:20:50 -0500

    Added code to log into Discord

--- Bot.cs
+++ Bot.cs
@@ -8,22 +8,27 @@ using TwitchLib.Client.Models;
 using TwitchLib.Communication.Clients;
 using TwitchLib.Communication.Models;
 using Discord.WebSocket;
+using Discord;
 
 namespace CoraCatte
 {
     public class Bot
     {
         private static XmlDocument config;
-        private static TwitchClient client;        
+        private static TwitchClient twitchClient;
+        private static DiscordSocketClient discordClient;
         private static string twitchUsername;
         private static string twitchChannel;
         private static string twitchAccessToken;
+        private static string discordAccessToken;
 
         public static XmlDocument Config { get => config; set => config = value; }
-        public static TwitchClient Client { get => client; set => client = value; }
+        public static TwitchClient TwitchClient { get => twitchClient; set => twitchClient = value; }
+        public static DiscordSocketClient DiscordClient { get => discordClient; set => discordClient = value; }
         public static string TwitchUsername { get => twitchUsername; set => twitchUsername = value; }
         public static string TwitchChannel { get => twitchChannel; set => twitchChannel = value; }
         public static string TwitchAccessToken { get => twitchAccessToken; set => twitchAccessToken = value; }
+        public static string DiscordAccessToken { get => discordAccessToken; set => discordAccessToken = value; }
 
         public Bot()
         {
@@ -32,23 +37,41 @@ namespace CoraCatte
             TwitchUsername = Config.SelectSingleNode("config/twitch.username").InnerText;
             TwitchChannel = Config.SelectSingleNode("config/twitch/channel").InnerText;
             TwitchAccessToken = Config.SelectSingleNode("config/twitch/accessToken").InnerText;
+            DiscordAccessToken = Config.SelectSingleNode("config/discord/accessToken").InnerText;
 
-            ConnectionCredentials credentials = new ConnectionCredentials(TwitchUsername, TwitchAccessToken);
+            ConnectionCredentials twitchCredentials = new ConnectionCredentials(TwitchUsername, TwitchAccessToken);
             var clientOptions = new ClientOptions
             {
                 MessagesAllowedInPeriod = 750,
                 ThrottlingPeriod = TimeSpan.FromSeconds(30)
             };
             WebSocketClient customClient = new WebSocketClient(clientOptions);
-            Client = new TwitchClient(customClient);
-            Client.Initialize(credentials, TwitchChannel);
+            TwitchClient = new TwitchClient(customClient);
+            TwitchClient.Initialize(twitchCredentials, TwitchChannel);
 
-            Client.OnMessageReceived += Client_OnMessageReceived;
+            TwitchClient.OnMessageReceived += Client_OnMessageReceived;
         }
 
         private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
         {
             // TODO: Add client message received function
+        }
+
+        public static async Task LoadDiscord()
+        {
+            DiscordClient = new DiscordSocketClient();
+            DiscordClient.Log += DiscordLog;
+
+            await DiscordClient.LoginAsync(TokenType.Bot, DiscordAccessToken);
+            await DiscordClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task DiscordLog(LogMessage logMessage)
+        {
+            Console.WriteLine(logMessage.ToString());
+            return Task.CompletedTask;
         }
     }
 }
