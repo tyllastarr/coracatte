de5c7b7d7b843fb6c877bbb963be1c6890cce1b7
Tyll'a <psduckie@yahoo.com>
2025-05-10 22:23:17 -0400

    Added blacklist for scalability

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const tmi = require("tmi.js");
 const player = require("node-wav-player");
 const discord = require("discord.js");
 const discordToken = require("./config.json");
+const blacklist = require("./blacklist.json")
 
 const twitchClient = new tmi.Client({
     connection: {
@@ -27,25 +28,25 @@ discordClient.login(discordToken.token);
 
 twitchClient.on("message", (channel, tags, message, self) => {
     var fullMessage;
-    switch (tags["display-name"]) {
-        case "KatCatte":
-            break;
-        case "Sery_Bot":
-            break;
-        case "Tylla":
-            break;
-        case "StreamElements":
-            break;
-        default:
-            player.play({
-                path: "./meow.wav",
-            }).then(() => {
-                fullMessage = ("Meow!  " + tags["display-name"] + " said \"" + message + "\"");
-                console.log(fullMessage);
-                discordClient.channels.cache.get("1047634550303506472").send(fullMessage);
-            }).catch((error) => {
-                console.error(error);
-            });
-            break;
+    var onBlacklist;
+
+    onBlacklist = false;
+    
+    blacklist.blacklist.forEach(function(blacklistItem) {
+        if(tags["display-name"] == blacklistItem) {
+            onBlacklist = true;
+        }
+    });
+
+    if(onBlacklist == false) {
+        player.play({
+            path: "./meow.wav",
+        }).then(() => {
+            fullMessage = ("Meow!  " + tags["display-name"] + " said \"" + message + "\"");
+            console.log(fullMessage);
+            discordClient.channels.cache.get("1047634550303506472").send(fullMessage);
+        }).catch((error) => {
+            console.error(error);
+        });
     }
 });
